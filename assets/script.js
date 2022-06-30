9bd408bd00777dfa526fedfac3a5c2cbcc5d81ae
Star97-dev <satriaanugerah94@gmail.com>
2022-06-30 20:38:29 +0700

    Update file

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,3 @@ let img = document.querySelector('.imgProfile');
 img.addEventListener('click', () => {
     alert("Ini foto profil saya");
 })
-
-let imgAb = document.querySelector('.imgAbout');
-imgAb.addEventListener('click', () => {
-    alert('Ini foto lagi ketiduran, waktu kelas di kampus wkwk');
-})
